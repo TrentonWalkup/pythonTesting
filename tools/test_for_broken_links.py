7669a5363bebcc2f5f6b1e02fd9d124294a44397
Trenton Walkup <trentonwalkup@Trentons-MacBook-Pro.local>
2020-02-06 18:57:38 -0600

    wip

--- tools/test_for_broken_links.py
+++ tools/test_for_broken_links.py
@@ -1,23 +1,21 @@
 from bs4 import BeautifulSoup, SoupStrainer
+from assertpy import assert_that
 import requests
 
-pageUrl = "https://zapier.com"
+pageUrl = "http://the-internet.herokuapp.com/login"
 
 page = requests.get(pageUrl)
 data = page.text
-soup = BeautifulSoup(data)
+soup = BeautifulSoup(data, 'html.parser')
 
 linksArry = []
 newArry = []
+
 for hrefs in soup.find_all('a'):
     linksArry.append(hrefs.get('href'))
     for a in linksArry:
-        newArry.append(pageUrl+str(a))
-
-
-
-url_responses = {}
+        newArry.append(str(a))
 
 for link in newArry:
     temp_r = requests.get(link)
-    url_responses[link] = temp_r.status_code
+    assert_that(temp_r.status_code).is_equal_to(200)
